82353516d4e73f1e35d32cf22fa3888ea48183dc
lekenamp <114669343+lekenamp@users.noreply.github.com>
2023-03-01 11:39:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/*const featuresLink = document.querySelector('.navbar .features-link');
+const dropdown = document.querySelector('.dropdown');
+const icon = featuresLink.querySelector('.icon');
+
+featuresLink.addEventListener('click', function(event){
+    event.preventDefault();
+    dropdown.classList.toggle('active');
+    icon.src = (dropdown.classList.contains('active')) ? 'images/icon-arrow-up.svg' : 'images/icon-arrow-down.svg';
+});*/
